2218bc7b1c3e34d9ee59a1f291ea3600cf33ff85
Jborro98 <96505624+Jborro98@users.noreply.github.com>
2023-11-13 07:39:03 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  var skills = [
+    { name: 'HTML', level: 5 },
+    { name: 'CSS', level: 4 },
+    { name: 'JS', level: 3 }
+  ];
+  return {
+    skills: skills
+  };
+}
